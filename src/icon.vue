8542753bd4a4ac396edbb02a9b856aa15af7a901
Frank <2521647197@qq.com>
2022-04-28 17:40:53 +0800

    封装icon组件

--- src/icon.vue
+++ src/icon.vue
@@ -1,13 +1,19 @@
 <template>
-<div>icon</div>
+  <svg  class="r-icon" aria-hidden="true">
+    <use :xlink:href="`#i-${name}`"></use>
+  </svg>
 </template>
 
 <script>
 export default {
-  name: "icon"
+  name: "r-icon",
+  props:["name"]
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.r-icon {
+  width: 1em;
+  height: 1em;
+}
 </style>
