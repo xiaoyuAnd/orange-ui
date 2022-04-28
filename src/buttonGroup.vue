236eaabbe3817ba5c58fbfc46e66212d9a01f4f0
Frank <2521647197@qq.com>
2022-04-28 18:16:10 +0800

    buttonGroup样式完成

--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -1,13 +1,30 @@
-<template>
-<div>buttonGroup</div>
-</template>
 
-<script>
-export default {
-  name: "buttonGroup"
+}
+}
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  & > .r-button {
+    border-radius: 0;
+    &:not(:first-child){
+      margin-left: -1px;
+    }
+    &:first-child {
+      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius);
+    }
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
 </style>
