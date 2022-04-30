c05023a41d4e3c0f053416b1579568a34947062d
Frank <2521647197@qq.com>
2022-04-30 16:51:47 +0800

    写测试用例

---
+++ test/input.test.js
@@ -0,0 +1,48 @@
+
+
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+    it('存在.', () => {
+        expect(Input).to.be.ok
+    })
+    it('props/v.', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                value: '1234'
+            }
+        }).$mount()
+        const useElement = vm.$el.querySelector('input')
+        expect(useElement.value).to.equal('1234')
+        vm.$destroy()
+    })
+    it('props/p.', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                placeholder:'1234'
+            }
+        }).$mount()
+        const useElements = vm.$el.querySelectorAll('use')
+        expect(useElements.placeholder).to.equal('1234')
+        vm.$destroy()
+    })
+    it('props/p.', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                disabled:true
+            }
+        }).$mount()
+        const useElements = vm.$el.querySelectorAll('input')
+        expect(useElements.disabled).to.equal('true')
+        vm.$destroy()
+    })
+
+})
