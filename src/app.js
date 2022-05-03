681bd5ccbe36046b9491fc4b8a0dd85842548ef2
Frank <2521647197@qq.com>
2022-05-03 10:57:12 +0800

    row col 雏形完成

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./button"
 import Icon from "./icon.vue"
 import ButtonGroup from "./buttonGroup"
 import Input from "./input"
+import Col from "./o-col"
+import Row from "./o-row"
 
 Vue.component('o-button',Button)
 Vue.component('o-icon',Icon)
 Vue.component('o-button-group',ButtonGroup)
 Vue.component('o-input',Input)
+Vue.component('o-col',Col)
+Vue.component('o-row',Row)
 new Vue({
     el:'#app',
     data:{
---
+++ src/o-col.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OCol",
+  props:{
+    span:{
+      type:[String,Number]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.col {
+  border: 1px solid #ccc;
+  background: orange;
+  width: 50%;
+  height: 100px;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: calc(($n / 24) * 100%);
+    }
+  }
+}
+</style>
