5a596aea7d5cb6860f8cb90d4c24d41b852fe3b8
Frank <2521647197@qq.com>
2022-05-04 15:40:33 +0800

    把 header content sider footer layout 注册为全局组件，加上了slot

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from "./buttonGroup"
 import Input from "./input"
 import Col from "./o-col"
 import Row from "./o-row"
+import Layout from "./l-layout"
+import Header from "./l-header"
+import Content from "./l-content"
+import Sider from "./l-sider"
+import Footer from "./l-footer"
 
 Vue.component('o-button',Button)
 Vue.component('o-icon',Icon)
@@ -12,6 +17,12 @@ Vue.component('o-button-group',ButtonGroup)
 Vue.component('o-input',Input)
 Vue.component('o-col',Col)
 Vue.component('o-row',Row)
+
+Vue.component('o-layout',Layout)
+Vue.component('o-content',Content)
+Vue.component('o-sider',Sider)
+Vue.component('o-header',Header)
+Vue.component('o-footer',Footer)
 new Vue({
     el:'#app',
     data:{
